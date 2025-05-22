54fcd59131b5b1987246bd2320e0b4acc72c210d
raksmakafo <119816635+raksmakafo@users.noreply.github.com>
2025-05-22 21:03:52 +0300

    new fitch

--- My project/Assets/Scripts/CellSCR.cs
+++ My project/Assets/Scripts/CellSCR.cs
@@ -1,6 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NewEmptyCSharpScript
+public class CellSCR : MonoBehaviour
 {
-    
+
+    public int state;
+
+    public Color normCol, pathCol; 
+    void Start()
+    {
+        
+    }
+
+    public void SetState(int i)
+    {
+        state = i;
+
+        if (i == 0)
+        {
+            GetComponent<Image>().color = normCol;
+        }
+        if (i == 1)
+        {
+            GetComponent<Image>().color = pathCol;
+        }
+     
+    }
+
 }
--- My project/Assets/Scripts/GameControllerScript.cs
+++ My project/Assets/Scripts/GameControllerScript.cs
@@ -1,6 +1,31 @@
 using UnityEngine;
 
+
 public class GameControllerScript : MonoBehaviour
 {
-    
+    public int cellCount;
+
+    public GameObject cellPref;
+    public Transform cellGroup;
+
+    void Start()
+    {
+        // Create the cells at the start of the game
+        CreateCells();
+    }
+    void CreateCells()
+    {
+        for(int i = 0; i < cellCount; i++)
+        {
+            GameObject tmpCell = Instantiate(cellPref);
+            tmpCell.transform.SetParent(cellGroup, false);
+            tmpCell.GetComponent<CellSCR>().SetState(0);
+        }
+    }
+
+    void CreatePath()
+    {
+
+    }
+
 }
