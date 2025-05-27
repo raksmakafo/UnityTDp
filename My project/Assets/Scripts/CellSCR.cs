ba45c7acdc58e4fcbf5124e66c8c8cbc7db36be3
Sq4ad <ilinaleksej380@gmail.com>
2025-05-27 17:50:59 +0300

    MAP

--- My project/Assets/Scripts/CellSCR.cs
+++ My project/Assets/Scripts/CellSCR.cs
@@ -1,30 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CellSCR : MonoBehaviour
 {
 
-    public int state, id;
-
-    public Color normCol, pathCol; 
-    void Start()
-    {
-        
-    }
-
-    public void SetState(int i)
-    {
-        state = i;
-
-        if (i == 0)
-        {
-            GetComponent<Image>().color = normCol;
-        }
-        if (i == 1)
-        {
-            GetComponent<Image>().color = pathCol;
-        }
-     
-    }
+    public bool isGround;
 
 }
--- My project/Assets/Scripts/GameControllerScript.cs
+++ My project/Assets/Scripts/GameControllerScript.cs
@@ -1,43 +1,11 @@
-using NUnit.Framework;
-using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
+
 
 
 public class GameControllerScript : MonoBehaviour
 {
-    public int cellCount;
 
-    int[] pathID = { 59, 49, 39, 29, 19, 18, 17, 16, 26, 36, 35, 34, 33, 32, 22, 12 };
-
-    List<CellSCR> AllCells = new List<CellSCR>();
-
-    public GameObject cellPref;
-    public Transform cellGroup;
-
-    void Start()
-    {
-        // Create the cells at the start of the game
-        CreateCells();
-        CreatePath();
-    }
-    void CreateCells()
-    {
-        for(int i = 0; i < cellCount; i++)
-        {
-            GameObject tmpCell = Instantiate(cellPref);
-            tmpCell.transform.SetParent(cellGroup, false);
-            tmpCell.GetComponent<CellSCR>().id = i + 1;
-            tmpCell.GetComponent<CellSCR>().SetState(0);
-            AllCells.Add(tmpCell.GetComponent<CellSCR>());
-        }
-    }
-
-    void CreatePath()
-    {
-        for(int i = 0; i < pathID.Length; i++)
-        {
-         AllCells[pathID[i] - 1].SetState(1);
-        }
-    }
 
 }
