053c7b3d8cb3abf0d6122f71fb2175b87dd2073b
raksmakafo <119816635+raksmakafo@users.noreply.github.com>
2025-05-27 13:51:11 +0300

    apdate puth

--- My project/Assets/Scripts/GameControllerScript.cs
+++ My project/Assets/Scripts/GameControllerScript.cs
@@ -1,11 +1,15 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
 
 
 public class GameControllerScript : MonoBehaviour
 {
     public int cellCount;
 
-    int[] pathID = { };
+    int[] pathID = { 59, 49, 39, 29, 19, 18, 17, 16, 26, 36, 35, 34, 33, 32, 22, 12 };
+
+    List<CellSCR> AllCells = new List<CellSCR>();
 
     public GameObject cellPref;
     public Transform cellGroup;
@@ -24,12 +28,16 @@ public class GameControllerScript : MonoBehaviour
             tmpCell.transform.SetParent(cellGroup, false);
             tmpCell.GetComponent<CellSCR>().id = i + 1;
             tmpCell.GetComponent<CellSCR>().SetState(0);
+            AllCells.Add(tmpCell.GetComponent<CellSCR>());
         }
     }
 
     void CreatePath()
     {
-
+        for(int i = 0; i < pathID.Length; i++)
+        {
+         AllCells[pathID[i] - 1].SetState(1);
+        }
     }
 
 }
